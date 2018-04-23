67e923fc69a1e46c9779278a73ebebc104f980eb
shanmuzhuo <shanmuzhuo@foxmail.com>
2018-04-24 01:09:34 +0800

    second comm

--- zheng-cms/zheng-cms-admin/src/main/java/com/zheng/cms/admin/controller/manage/CmsDayongController.java
+++ zheng-cms/zheng-cms-admin/src/main/java/com/zheng/cms/admin/controller/manage/CmsDayongController.java
@@ -1,10 +1,19 @@
 package com.zheng.cms.admin.controller.manage;
 
+import com.zheng.cms.dao.model.CmsTopic;
+import com.zheng.cms.dao.model.CmsTopicExample;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import com.zheng.common.base.BaseController;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
 
 
 /**
@@ -12,14 +21,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * created by zhuozhenxun on 20118-04-20
  */
 @Controller
+@Api(value = "景点", description = "景点管理")
 @RequestMapping("/manage/spot")
 public class CmsDayongController extends BaseController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(CmsArticleController.class);
 
-    @RequestMapping(value = "/demo")
-    public String sayHello(){
+    /**
+     * 景点列表
+     *
+     * @return
+     */
+    @ApiOperation(value = "景点管理主页")
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public String sayHello() {
         System.out.println("hello 123456");
-        return thymeleaf("/index");
+        return "/manage/dayong/index.jsp";
+    }
+
+    @ApiOperation(value = "新增景点")
+    @RequiresPermissions("cms:dayong:add")
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    public String add(ModelMap modelMap) {
+        return "/manage/dayong/add.jsp";
     }
 }
